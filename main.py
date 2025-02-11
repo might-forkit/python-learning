6569690d60e207cff671f7923073a38a793ddc9f
might-forkit <shriya.forwork@gmail.com>
2025-02-11 20:56:09 +0530

    basics of python

---
+++ main.py
@@ -0,0 +1,37 @@
+#Variables
+
+#Strings
+first_name = 'Shriya'
+food = 'Pizza'
+email = 'shriya123thic.com'
+
+print(f'Hello {first_name}')
+print(f'You like {food}')
+print(f'Your email is {email}')
+
+#Integers
+age = 20
+no_of_students = 45
+
+#Float
+price = 10.99
+gpa = 3.4
+distance = 5.5
+
+print(f'You are {age} years old')
+print(f'Your class has {no_of_students} students')
+print(f'Your gpa is {gpa}')
+print(f'You ran {distance}km')
+
+#Boolean - True/False
+
+is_student = True
+
+print(f'Are you a student? - {is_student}')
+
+
+
+
+
+
+
